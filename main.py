b976e85312916da8b2ac29c5f7be662365d31684
Bryce Washington <brlw223@gmail.com>
2024-09-28 22:51:12 -0400

    Connects to database and can select all

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import pymysql
+
+app = FastAPI()
+
+db_connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='dbuserdbuser',
+    db='expense_tracker_users'
+)
+
+db_cursor = db_connection.cursor()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/select")
+async def select():
+    db_cursor.execute("select * from users")
+    return db_cursor.fetchall()
